746913ccae8dcacc0e673a1ec34a9ef3a345d1cf
dvankevich <dvankevich@gmail.com>
2026-01-10 14:27:40 +0200

    add future flags on BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import { HelmetProvider } from 'react-helmet-async';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
-import { App } from './components/App';
-import { store, persistor } from './redux/store';
-import 'modern-normalize';
-import './index.css';
+import React from "react";
+import { HelmetProvider } from "react-helmet-async";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { App } from "./components/App";
+import { store, persistor } from "./redux/store";
+import "modern-normalize";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter
+          future={{
+            v7_relativeSplatPath: true,
+            v7_startTransition: true,
+          }}
+        >
           <HelmetProvider>
             <App />
           </HelmetProvider>
